8a19870e79637b2d26bea78cc7f139a8fb5d2830
sanzmoses <sanzmoses@gmail.com>
2024-05-23 21:56:43 +0800

    add buttongs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="outer-container">
     <div class="pa-5">
       <router-view v-slot="{ Component }">
         <transition
@@ -14,26 +14,46 @@
     </div>
     
     <PlayerNameDialog />
+
+    <SeamlessNav class="fix center-left" :label="'How to play'" />
+    <SeamlessNav class="fix upper-right" :label="'Hall of fame'" />
+    <SeamlessNav class="fix upper-left" :label="'About the game'" />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
 import PlayerNameDialog from "./components/PlayerNameDialog.vue"
+import SeamlessNav from "./components/SeamlessNav.vue"
 
 export default {
   name: 'App',
   components: {
-    PlayerNameDialog
+    PlayerNameDialog,
+    SeamlessNav
   },
-  setup() {
-  }
+  setup() { }
 }
 </script>
 
 <style lang="scss">
 body {
-  scrollbar-width: thin;          /* "auto" or "thin" */
-  scrollbar-color: blue orange;   /* scroll thumb and track */ 
+  scrollbar-width: thin; /* "auto" or "thin" */
+  scrollbar-color: blue orange; /* scroll thumb and track */
+  position: relative; 
+}
+
+.fix {
+  position: absolute;
+  top: 0;
+
+  &.center-left {
+    top: 50%;
+    transform: translateX(-50px) rotate(-90deg);
+  }
+
+  &.upper-right {
+    right: 0;
+    left: auto;
+  }
 }
 </style>
--- src/components/SeamlessNav.vue
+++ src/components/SeamlessNav.vue
@@ -1,25 +1,17 @@
 <template>
   <div>
-    <transition
-      appear
-      enter-active-class="animated fadeInRight"
-      leave-active-class="animated fadeOutRight"
-    >
-      <q-btn 
-        v-if="!seamless"
-        @click="seamless = !seammless" 
-        icon="format_list_bulleted" 
-        text-color="black" 
-        color="primary"
-        size="md"
-        push 
-        round 
-      />
-    </transition>
+    <q-btn 
+      @click="seamless = !seammless" 
+      :label="label"
+      text-color="white" 
+      color="primary"
+      class="text-lowercase"
+      flat
+    />
 
     <q-dialog 
       v-model="seamless" 
-      position="right"
+      position="left"
       seamless 
     >
       <q-card style="width: 350px">
@@ -27,7 +19,7 @@
 
         <q-card-section class="row items-center no-wrap">
           <div>
-            <h5 class="q-ma-none">Table of Contents</h5>
+            <h5 class="q-ma-none">How to play</h5>
           </div>
 
           <q-space />
@@ -35,17 +27,6 @@
         </q-card-section>
 
         <q-card-section class="q-px-lg q-pt-none q-pb-lg">
-          <template v-for="link in compagestore.links" :key="'links-'+link">
-            <q-btn 
-              :outline="compagestore.active == link"
-              :flat="compagestore.active != link"
-              class="text-capitalize q-ma-xs" 
-              color="warning" 
-              :label="link"
-              @click="handleClick(link)"
-            />
-          </template>
-          
         </q-card-section>
       </q-card>
     </q-dialog>
@@ -54,49 +35,28 @@
 
 <script>
 import { ref, watchEffect  } from 'vue'
-import { useComponentsPageStore } from '/src/stores/ComponentsPageStore'
 
 export default {
   name: "seamless nav",
   props: {
-    is_fixed: {
-      type: Boolean
+    label: {
+      type: String
     }
   },
   setup () {
-    const compagestore = useComponentsPageStore()
-    const setActive = compagestore.SET_ACTIVE
-    const setScrollTo = compagestore.SET_SCROLL_TO
 
     const progress = ref(0)
-
     watchEffect(async () => {
-      if(compagestore.scroll_top) {
-        const innerHeight = window.innerHeight;
-        const scroll_height = document.documentElement.scrollHeight - innerHeight;
-        const current_scroll = compagestore.scroll_top;
-        const decimal = (current_scroll / scroll_height).toFixed(2);
-        
-        progress.value = JSON.parse(decimal)
-      }
+      
     })
 
     return {
       seamless: ref(false),
-      compagestore,
-      setActive,
-      setScrollTo,
       progress
     }
   },
-  methods: {
-    handleClick(link) {
-      this.setActive(link)
-      this.setScrollTo(true);
-    }
-  }
 }
 </script>
 
-<style lang="">
+<style lang="scss">
 </style>
